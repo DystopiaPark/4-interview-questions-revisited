4343e5f4e6be525d39860323e137eb3bce4441b6
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-03-23 23:41:54 +0100

    separated Form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Form = ({
+  createQuestion,
+  question,
+  answer,
+  setQuestion,
+  setAnswer,
+  option,
+  setOption,
+}) => {
+  return (
+    <form onSubmit={createQuestion}>
+      <input
+        type="text"
+        placeholder="question"
+        onChange={(e) => setQuestion(e.target.value)}
+        value={question}
+      />
+      <textarea
+        className="textarea-answer"
+        placeholder="answer"
+        onChange={(e) => setAnswer(e.target.value)}
+        value={answer}
+      ></textarea>
+      <select value={option} onChange={(e) => setOption(e.target.value)}>
+        <option value="html">HTML</option>
+        <option value="css">CSS</option>
+        <option value="js">JS</option>
+        <option value="react">React</option>
+        <option value="cs">CS</option>
+      </select>
+      <button type="submit" value="Add question" className="submit-btn">
+        Add New Interview Question
+      </button>
+    </form>
+  );
+};
+
+export default Form;
